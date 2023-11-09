b915b97405d39efe73a6525105095d6eb3cff5ee
Yassine Gherbi <yassine@hexelnet.com>
2023-11-09 03:30:47 +0100

    refactor: reuse fetch

--- src/api.ts
+++ src/api.ts
@@ -3,6 +3,7 @@ import { SearchParams, JSONResponse, PreparedResponse } from './types'
 import prepareResponse from './utils/prepareResponse'
 
 const API_URL = 'https://openwheels.nl/api/'
+const LIMIT_RESULTS: number | null = 100
 
 type ApiConfig = {
   method: 'search.map' | string
@@ -10,7 +11,23 @@ type ApiConfig = {
   refetchOnPropsChange?: any[]
 }
 
-const LIMIT_RESULTS: number | null = 100
+function fetchResources(config: Pick<ApiConfig, 'method' | 'params'>) {
+  return fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      'x-ref': 'http://localhost:9009',
+      'X-Simple-Auth-App-Id':
+        '28_c3VwZXJzZWNyZXRteXdoZWVsc2NvZGluZ3Rlc3RzY3JldDhuMjdxdGc5ODdxM3R5',
+    },
+    body: JSON.stringify({
+      jsonrpc: '2.0',
+      id: 0,
+      method: config.method,
+      params: config.params,
+    }),
+  })
+}
 
 export const useApi = (config: ApiConfig) => {
   const [isLoading, setIsLoading] = useState<boolean>(false)
@@ -24,22 +41,7 @@ export const useApi = (config: ApiConfig) => {
     ;(async () => {
       setIsLoading(true)
 
-      const res = await fetch(API_URL, {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json',
-          'x-ref': 'http://localhost:9009',
-          'X-Simple-Auth-App-Id':
-            '28_c3VwZXJzZWNyZXRteXdoZWVsc2NvZGluZ3Rlc3RzY3JldDhuMjdxdGc5ODdxM3R5',
-        },
-        body: JSON.stringify({
-          jsonrpc: '2.0',
-          id: 0,
-          method: config.method,
-          params: config.params,
-        }),
-      })
-
+      const res = await fetchResources(config)
       const json = await res.json()
 
       const data = {
@@ -64,23 +66,9 @@ export const useApi = (config: ApiConfig) => {
 
 const api = {
   fetchResources: async (config: ApiConfig) => {
-    const response = await fetch(API_URL, {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-        'x-ref': 'http://localhost:9009',
-        'X-Simple-Auth-App-Id':
-          '28_c3VwZXJzZWNyZXRteXdoZWVsc2NvZGluZ3Rlc3RzY3JldDhuMjdxdGc5ODdxM3R5',
-      },
-      body: JSON.stringify({
-        jsonrpc: '2.0',
-        id: 0,
-        method: config.method,
-        params: config.params,
-      }),
-    })
+    const res = await fetchResources(config)
 
-    const json = await response.json()
+    const json = await res.json()
     const data = {
       ...json,
       result: {
